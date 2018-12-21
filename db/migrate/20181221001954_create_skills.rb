d843b69f53df5301a3294318837e06014bc44bfc
romainr88 <romainr88dev@gmail.com>
2018-12-21 00:22:47 +0000

    Added skills db

---
+++ db/migrate/20181221001954_create_skills.rb
@@ -0,0 +1,12 @@
+class CreateSkills < ActiveRecord::Migration[5.2]
+  def change
+    create_table :skills do |t|
+      t.string :type
+      t.string :title
+      t.text :description
+      t.string :icon
+
+      t.timestamps
+    end
+  end
+end
