0b1857922bfc39c9ee983173e66ef4c21f9e16dd
romainr88 <romainr88dev@gmail.com>
2019-01-03 04:14:21 +0000

    Updated AddDescriptionToPortfolio file

--- db/migrate/20181225034438_add_description_to_portfolio.rb
+++ db/migrate/20181225034438_add_description_to_portfolio.rb
@@ -1,5 +1,6 @@
 class AddDescriptionToPortfolio < ActiveRecord::Migration[5.2]
   def change
+    add_column :portfolios, :description, :text
     add_column :portfolios, :website, :text
   end
 end
